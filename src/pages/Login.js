06c4e55c37856e9380ed73d9c3c79f8fe6a9f7a6
Adler Coelho Santos <adler.santos@ftd.com.br>
2019-08-11 10:46:33 -0300

    Init commit..

---
+++ src/pages/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './../assets/logo.svg';
+import './Login.css';
+
+function Login() {
+    return(
+        <div className="login-container">
+            <form>
+                <img src={ logo } alt="Tindev" />
+                <input 
+                    placeholder="Digite seu usuário do GitHub"
+                />
+                <button>Enviar</button>
+            </form>
+        </div>
+    );
+}
+export default Login;
